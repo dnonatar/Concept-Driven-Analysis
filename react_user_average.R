16628f5bf3cb7f1169f4813b0a22910f8c23b96d
Koonchanok, Ratanond <rkoonch@iu.edu>
2020-09-09 02:26:22 -0400

    update ggplot

--- react_user_average.R
+++ react_user_average.R
@@ -10,9 +10,9 @@ library(ggplot2)
 
 ## pick react
 
-this_react = "REASON"
+this_react = "HYPOTHESIS"
 
-## phase 1
+## Prediction
 
 data = merged_df_1 
 
@@ -33,13 +33,12 @@ df_1 = count_data[count_data$REACT== this_react,]
 df_1 = df_1[c('USER','REACT','percent')]
 
 
-### Filling in for Hyposthesis
+### Filling in for missing values
 if (this_react == 'HYPOTHESIS'){
   USER = c('1.1')
   REACT = c('HYPOTHESIS')
   percent = c(0)
   df_1 = rbind(data.frame(USER,REACT,percent),df_1)
-  #ggplot(data = df_1, aes(x=USER, y=percent))+geom_col()+ylim(0,45)
 }
 
 if (this_react == 'POSTHYPOTHESIS'){
@@ -47,22 +46,37 @@ if (this_react == 'POSTHYPOTHESIS'){
   REACT = c('POSTHYPOTHESIS')
   percent = c(0)
   df_1 = rbind(data.frame(USER,REACT,percent),df_1)
-  
-  #ggplot(data = df_1, aes(x=USER, y=percent))+geom_col()+ylim(0,21)
-}
-
-if (this_react == 'OBSERVATION'){
-  #ggplot(data = df_1, aes(x=USER, y=percent))+geom_col()+ylim(0,40)
 }
 
 if (this_react == 'REVBELIEF'){
-  USER = c('1.4','1.9')
+  USER = c('1.4')
   REACT = c('REVBELIEF')
   percent = c(0)
   df_1 = rbind(data.frame(USER,REACT,percent),df_1)
 }
 
-## phase 2
+if (this_react == 'SURPRISE'){
+  USER = c('1.4','1.15')
+  REACT = c('SURPRISE')
+  percent = c(0)
+  df_1 = rbind(data.frame(USER,REACT,percent),df_1)
+}
+
+if (this_react == 'GOAL'){
+  USER = c('1.1','1.4')
+  REACT = this_react
+  percent = c(0)
+  df_1 = rbind(data.frame(USER,REACT,percent),df_1)
+}
+
+if (this_react == 'REASON'){
+  USER = c('1.1')
+  REACT = this_react
+  percent = c(0)
+  df_1 = rbind(data.frame(USER,REACT,percent),df_1)
+}
+
+## Standard
 
 data = merged_df_2
 
@@ -80,26 +94,43 @@ for (user in unique(count_by_user$USER)) {
 count_data$percent = count_data$count / count_data$total * 100
 
 df_2 = count_data[count_data$REACT==this_react,]
-
-if (this_react == 'POSTHYPOTHESIS'){
-  #ggplot(data = df_2, aes(x=USER, y=percent))+geom_col()+ylim(0,21)
-}
-  
-
-if (this_react == 'HYPOTHESIS'){
-  #ggplot(data = df_2, aes(x=USER, y=percent))+geom_col()+ylim(0,45)
-}
-
-if (this_react == 'OBSERVATION'){
-  #ggplot(data = df_2, aes(x=USER, y=percent))+geom_col()+ylim(0,40)
-}
+df_2 = df_2[c('USER','REACT','percent')]
 
 if (this_react == 'REVBELIEF'){
   USER = c('2.4','2.10')
   REACT = c('REVBELIEF')
   percent = c(0)
-  df_1 = rbind(data.frame(USER,REACT,percent),df_1)
+  df_2 = rbind(data.frame(USER,REACT,percent),df_2)
 }
 
-df_both = data.frame(with_predictions = c("Yes","No"), percentage = c(mean(df_1$percent),mean(df_2$percent)))
-ggplot(data = df_both, aes(x=with_predictions, y=percentage))+geom_col()+ggtitle(this_react)
+if (this_react == 'SURPRISE'){
+  USER = c('2.4','2.6','2.12')
+  REACT = this_react
+  percent = c(0)
+  df_2 = rbind(data.frame(USER,REACT,percent),df_2)
+}
+
+if (this_react == 'GOAL'){
+  USER = c('2.12')
+  REACT = this_react
+  percent = c(0)
+  df_2 = rbind(data.frame(USER,REACT,percent),df_2)
+}
+
+##########
+
+df_both = data.frame(Group = c('P', 'S'), 
+                     percentage = c(mean(df_1$percent),mean(df_2$percent)), 
+                     CI = c(1.96*sd(df_1$percent)/sqrt(12), 1.96*sd(df_2$percent)/sqrt(12)))
+
+ggplot(data = df_both, aes(x=Group, y=percentage))+
+  geom_col(width = 0.5, fill = c("#edb89f","#9fcbed"), color="black")+
+  geom_errorbar(aes(ymin=percentage-CI, ymax=percentage+CI), width=.2, position=position_dodge(.9)) +
+  ggtitle("BELIEF UPDATE") + 
+  ylab("% of All Reactions")  +
+  theme_minimal() +
+  theme(aspect.ratio = 1.5/1) + 
+  theme(plot.title = element_text(color="black", size=14, face="bold.italic"),
+        axis.title.x = element_text(color="black", size=14),
+        axis.title.y = element_text(color="black", size=14),
+        axis.text=element_text(size=12, face="bold"))
