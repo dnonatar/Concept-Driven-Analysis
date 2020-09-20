cbf81e85870df553a19c8c03957f30e763fdcccf
Koonchanok, Ratanond <rkoonch@iu.edu>
2020-09-20 18:57:53 -0400

    normalize across condition

--- timespent.R
+++ timespent.R
@@ -70,15 +70,15 @@ timespent_CI_2 = sd(timespent_sum_2$timespent/timespent_sum_2$queries_count)*1.9
 timespent_explore = data.frame(Group = c('P','S'), 
                                time = c(timespent_avg_1,timespent_avg_2),
                                CI = c(timespent_CI_1,timespent_CI_2),
-                               Action = "Exploring Data"
+                               Action = "Looking at Data"
 )
 
 #### plot
 ggplot(data = timespent_total, aes(x=Group, y=time))+
   geom_col(width = 0.5, fill = c("#edb89f","#9fcbed"), color="black")+
   geom_errorbar(aes(ymin=time-CI, ymax=time+CI), width=.2, position=position_dodge(.9)) +
-  ggtitle("Total Time Spent") + 
-  ylab("Time Spent (sec)")  +
+  ggtitle("Average Time Spent") + 
+  ylab("Average Seconds per Query")  +
   theme_minimal() +
   theme(aspect.ratio = 1.5/1) + 
   theme(plot.title = element_text(color="black", size=14, face="bold.italic"),
@@ -86,6 +86,9 @@ ggplot(data = timespent_total, aes(x=Group, y=time))+
         axis.title.y = element_text(color="black", size=14),
         axis.text=element_text(size=12, face="bold"))
 
+
+timespent_total$low = timespent_total$time - timespent_total$CI
+timespent_total$high = timespent_total$time + timespent_total$CI
 
 
 time_by_mode = rbind(timespent_predict,timespent_explore)
@@ -95,11 +98,15 @@ ggplot(time_by_mode, aes(Action, y=time, fill=Group)) +
   geom_errorbar(aes(ymin=time-CI, ymax=time+CI), width=.2, position=position_dodge(0.6)) +
   scale_fill_manual(values=c("#edb89f","#9fcbed")) +
   theme_minimal() +
-  ggtitle("Time Spent by Actions") + 
-  ylab("Time Spent (sec)") +
+  ggtitle("Average Time Spent by Actions") + 
+  ylab("Average Seconds per Query") +
   xlab("Action")  + 
   theme(plot.title = element_text(color="black", size=14, face="bold.italic"),
         axis.title.x = element_text(color="black", size=14),
         axis.title.y = element_text(color="black", size=14),
         axis.text=element_text(size=12, face="bold",angle = 0, hjust = 0.5))
 
+time_by_mode$low = time_by_mode$time - time_by_mode$CI
+time_by_mode$high = time_by_mode$time + time_by_mode$CI
+
+time_by_mode
